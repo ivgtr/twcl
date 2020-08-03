876082805d1b87516dc880085fb5e9fe4b7baab5
ivgtr <gear.1112t.o@gmail.com>
2020-08-03 20:08:09 +0900

    :sparkles: add: ツイート処理の雛形を追加

--- src/commands/tweet.ts
+++ src/commands/tweet.ts
@@ -4,14 +4,26 @@ import {
   TWITTER_CONSUMER_SECRET_KEY
 } from '../configs/config.json'
 
-const client = new Twitter({})
+const client = new Twitter({
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET_KEY,
+  access_token_key: '',
+  access_token_secret: ''
+})
 
-const Tweet = async () => {
-  console.log('やあ')
-
-  await setTimeout(() => {
-    console.log('test')
-  }, 1000)
+const Tweet = async (): Promise<void> => {
+  const content = '帰る'
+  await client.post(
+    'statuses/update',
+    { status: content },
+    (error, tweet, response) => {
+      if (!error) {
+        console.log(`tweet success: ${tweet}`)
+      } else {
+        console.log(error)
+      }
+    }
+  )
 }
 
 export default Tweet
