302b099cd9aaf2426ef0cb9e7141ec0c7f2ef822
ivgtr <gear.1112t.o@gmail.com>
2020-08-03 20:07:00 +0900

    :sparkles: add: oauthの処理を追加

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -1,25 +1,80 @@
-import Twitter from 'twitter'
 import axios from 'axios'
 import crypto from 'crypto'
+import Datastore from 'nedb'
 import open from 'open'
+import OAuth, { RequestOptions, Options, Token } from 'oauth-1.0a'
+import prompts from 'prompts'
 import {
   TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET_KEY
+  TWITTER_CONSUMER_SECRET_KEY,
+  TWITTER_OAUTH_URL,
+  TWITTER_OAUTH_CALLBACK_URL
 } from '../configs/config.json'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const OAuth = require('oauth-1.0a')
-
-const oauthUrl = 'https://api.twitter.com/oauth'
-const oauthCallback = 'http://'
-
-const requestData = {
+const oauthUrl = TWITTER_OAUTH_URL
+const oauthCallback = TWITTER_OAUTH_CALLBACK_URL
+const requestData: RequestOptions = {
   url: `${oauthUrl}/request_token`,
   method: 'POST',
-  data: { oauthCallback }
+  data: { oauth_verifier: oauthCallback }
 }
+
+const inputUser = async (): Promise<boolean> => {
+  const { accessToken, accessTokenSecret, userName } = await prompts([
+    {
+      type: 'text',
+      name: 'accessToken',
+      message: '公開アクセストークンを入力してください'
+    },
+    {
+      type: 'text',
+      name: 'accessTokenSecret',
+      message: '秘密アクセストークンを入力してください'
+    },
+    {
+      type: 'text',
+      name: 'userName',
+      message: '表示名を入力してください'
+    }
+  ])
+
+  if (accessToken && accessTokenSecret && userName) {
+    console.log(`ようこそ、${userName}`)
+
+    try {
+      const db = await new Datastore({
+        filename: 'configs/setting.db'
+      })
+      await db.loadDatabase()
+      await db.insert(
+        {
+          user_selected: userName,
+          user_list: [
+            {
+              user_name: userName,
+              user_token: accessToken,
+              user_token_secret: accessTokenSecret
+            }
+          ]
+        },
+        (error, newDoc) => {
+          if (error !== null) {
+            console.error(error)
+          }
+          console.log(newDoc)
+        }
+      )
+      return true
+    } catch (err) {
+      console.log(`${err}\n不明なエラーです`)
+      return inputUser()
+    }
+  }
+  return inputUser()
+}
+
 export const Login = async (): Promise<void> => {
-  const options = {
+  const options: Options = {
     consumer: {
       key: TWITTER_CONSUMER_KEY,
       secret: TWITTER_CONSUMER_SECRET_KEY
@@ -29,18 +84,30 @@ export const Login = async (): Promise<void> => {
       return crypto.createHmac('sha1', key).update(baseString).digest('base64')
     }
   }
-  const oauth = await OAuth(options)
+  const getOauthInstance = (): OAuth => {
+    return new OAuth(options)
+  }
 
-  const res = await axios.post(
-    `${oauthUrl}/request_token`,
-    {},
-    {
-      headers: oauth.toHeader(oauth.authorize(requestData, {}))
+  try {
+    const responce = await axios.post(
+      `${oauthUrl}/request_token`,
+      {},
+      {
+        headers: getOauthInstance().toHeader(
+          getOauthInstance().authorize(requestData)
+        )
+      }
+    )
+    const token: Token = responce.data.split('&')[0].split('=')[1]
+    await open(`${oauthUrl}/authorize?oauth_token=${token}`)
+    if (await inputUser()) {
+      console.log('ok')
+    } else {
+      console.log('error!! もう一度初めからどうぞ')
     }
-  )
-  const token = res.data.split('&')[0].split('=')[1]
-
-  await open(`${oauthUrl}/authenticate?oauth_token=${token}&force_login=true`)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export const Logout = async () => {
