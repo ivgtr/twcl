1bff456dbf7b74fe6384ca8163c499bb0d490a24
ivgtr <gear.1112t.o@gmail.com>
2021-01-06 12:01:29 +0900

    :fire: remove: リファクタリングの為にファイルを除去

--- src/commands/index.ts
+++
@@ -1,9 +0,0 @@
-import tweet from './tweet'
-import timeline from './timeline'
-import list from './list'
-import user from './user'
-import login from './login'
-import logout from './logout'
-import search from './search'
-
-export { tweet, timeline, list, user, login, logout, search }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import cli from './cli'
-
-export default cli()
