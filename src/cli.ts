3951ed00c6ddc50b1582e4bf988d83e653bfa1cf
ivgtr <gear.1112t.o@gmail.com>
2021-01-26 15:35:55 +0900

    :sparkles: add: コマンドを一通り仮置き

--- src/cli.ts
+++ src/cli.ts
@@ -4,10 +4,10 @@ import updateNotifier from 'update-notifier'
 
 program
   .command('user')
-  .description('ユーザー操作')
-  .option('-a, --add', 'ユーザー追加')
-  .option('-d, --delete', 'ユーザー削除')
-  .option('-c, --change', 'ユーザー切り替え')
+  .description('User Add/Delete/Change Operation')
+  .option('-a, --add', 'Add a User')
+  .option('-d, --delete', 'Delete a User')
+  .option('-c, --change', 'Change a User')
   .action((options: { add?: boolean; delete?: boolean; change?: boolean }) => {
     console.log(options.add)
     console.log(options.delete)
@@ -15,8 +15,19 @@ program
   })
 program
   .command('tweet')
-  .description('ツイートする')
-  .option('-t, --tweet <tweet>', 'パパパっとツイート')
+  .description('Tweet now')
+  .option('-t, --tweet <TWEET>', 'Tweet shortcut')
+  .action((options: { tweet?: string }) => {
+    console.log(options.tweet)
+  })
+
+program
+  .command('show')
+  .description('Show the Timeline/List/User/Search')
+  .option('-t, --timeline', 'Show the your Timeline')
+  .option('-l, --list', 'Show the List')
+  .option('-u, --User <USER_ID>', 'Show the User')
+  .option('-s, --serch <SERCH_WORD>', 'Show the Search result')
   .action((options: { tweet?: string }) => {
     console.log(options.tweet)
   })
@@ -26,8 +37,8 @@ const main = (argv: string[]) => {
 
   program
     .name('twcl')
-    .version(pjson.version, '-v, --version', 'バージョンを確認')
-    .helpOption('-h, --help', 'コマンド一覧を表示')
+    .version(pjson.version, '-v, --version', "Show the twcl's version")
+    .helpOption('-h, --help', 'display help for command')
     .parse(argv)
 }
 
